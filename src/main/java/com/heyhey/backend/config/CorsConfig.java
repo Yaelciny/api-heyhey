58e4998711192d5415c97cd1f255749e5f28f328
Yaelciny <mag07yael@gmail.com>
2026-07-01 00:07:53 -0600

    Integracion de configuracion al servidor

---
+++ src/main/java/com/heyhey/backend/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.heyhey.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration // configuracion globales del servidor
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**") // Aplica el permiso a todas tus rutas
+                .allowedOrigins("http://localhost:3001") // Aquí le damos acceso
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Permisos de acciones
+                .allowedHeaders("*") // Permite enviar cualquier tipo de dato
+                .allowCredentials(true);
+    }
+}
